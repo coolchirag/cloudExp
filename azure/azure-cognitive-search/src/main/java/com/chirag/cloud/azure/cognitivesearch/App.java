6c3380839cc2bd4040ed939ae3cd7c589f26364f
chiragj <chirag.j@ezdi.us>
2021-06-15 12:31:36 +0530

    initial commits

---
+++ azure/azure-cognitive-search/src/main/java/com/chirag/cloud/azure/cognitivesearch/App.java
@@ -0,0 +1,9 @@
+package com.chirag.cloud.azure.cognitivesearch;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
---
+++ azure/azure-cognitive-search/src/test/java/com/chirag/azureCongnitiveSearach/AppTest.java
@@ -0,0 +1,14 @@
+package com.chirag.azureCongnitiveSearach;
+
+import org.junit.Test;
+
+import com.chirag.cloud.azure.cognitivesearch.App;
+
+public class AppTest {
+
+  @Test
+  public void testMain() {
+    App.main(null);
+  }
+
+}
