3860d6ffdc9440e75041d1951f4f610dd0166806
chiragjivani <chiragjivani@Laptop-238>
2022-03-31 14:53:23 +0530

    test commit

--- azure/azure-service/src/main/java/com/chirag/cloud/azure/service/AzureApp.java
+++ azure/azure-service/src/main/java/com/chirag/cloud/azure/service/AzureApp.java
@@ -3,9 +3,15 @@ package com.chirag.cloud.azure.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.core.publisher.Mono;
+
 @SpringBootApplication
 public class AzureApp {
     public static void main(String[] args) {
         SpringApplication.run(AzureApp.class, args);
+		/*
+		 * Mono<String> data = Mono.just("hello"); data.block();
+		 */
+        
     }
-}
+} 
